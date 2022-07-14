1bf132180d72c076f8075881f90f25c9561a7b97
OcimaOcram <marco.amico.pesci00@gmail.com>
2022-07-14 17:24:42 +0200

    esercizio completato

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class PageController extends Controller
 {
     public function index() {
         return view('welcome');
     }
     public function movie() {
-        $movies = \App\Movie::all();
-        dd($movies);
+        $movies = Movie::all();
+        return view('welcome' , compact('movies'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'PageController@index')->name('page');
-Route::get('/movies', 'PageController@movies')->name('movies');
+Route::get('/movies', 'PageController@movie')->name('movies');
